93fa4a247bc80860f701069f51816454af03a466
RMABID <mdabidaliimran@gmail.com>
2025-02-20 18:15:31 +0600

    post a new task

--- index.js
+++ index.js
@@ -21,16 +21,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const productsCollection = client.db("RS-Tails").collection("product");
-  
+    const taskCollection = client.db("ApplicationTask").collection("task");
+
+    app.post("/task", async (req, res) => {
+      const newTask = req.body;
+      const result = await taskCollection.insertOne(newTask);
+      res.send(result);
+    });
 
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log(
-    //   "Pinged your deployment. You successfully connected to MongoDB!"
-    // );
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
